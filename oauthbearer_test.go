8e14978c502d40ff168f7261298ee6a572ac8bcd
Frode Aannevik <frode.aa@gmail.com>
2019-06-25 01:08:19 +0200

    Add OAuthBearer tests

---
+++ oauthbearer_test.go
@@ -0,0 +1,52 @@
+package sasl_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/emersion/go-sasl"
+)
+
+func TestNewOAuthBearerClient(t *testing.T) {
+	c := sasl.NewOAuthBearerClient(&sasl.OAuthBearerOptions{
+		Username: "user@example.com",
+		Token:    "vF9dft4qmTc2Nvb3RlckBhbHRhdmlzdGEuY29tCg==",
+		Host:     "server.example.com",
+		Port:     143,
+	})
+
+	mech, ir, err := c.Start()
+	if err != nil {
+		t.Fatal("Error while starting client:", err)
+	}
+	if mech != "OAUTHBEARER" {
+		t.Error("Invalid mechanism name:", mech)
+	}
+
+	expected := []byte{110, 44, 97, 61, 117, 115, 101, 114,
+		64, 101, 120, 97, 109, 112, 108, 101, 46, 99, 111,
+		109, 44, 1, 104, 111, 115, 116, 61, 115, 101, 114,
+		118, 101, 114, 46, 101, 120, 97, 109, 112, 108, 101,
+		46, 99, 111, 109, 1, 112, 111, 114, 116, 61, 49, 52,
+		51, 1, 97, 117, 116, 104, 61, 66, 101, 97, 114, 101,
+		114, 32, 118, 70, 57, 100, 102, 116, 52, 113, 109,
+		84, 99, 50, 78, 118, 98, 51, 82, 108, 99, 107, 66,
+		104, 98, 72, 82, 104, 100, 109, 108, 122, 100, 71,
+		69, 117, 89, 50, 57, 116, 67, 103, 61, 61, 1, 1}
+	if bytes.Compare(ir, expected) != 0 {
+		t.Error("Invalid initial response:", ir)
+	}
+
+	challenge := []byte("eyJzdGF0dXMiOiJpbnZhbGlkX3Rva2VuIiwic2NvcGUiOiJleGFt" +
+		"cGxlX3Njb3BlIiwib3BlbmlkLWNvbmZpZ3VyYXRpb24iOiJodHRwczovL2V4YW1wbGUu" +
+		"Y29tLy53ZWxsLWtub3duL29wZW5pZC1jb25maWd1cmF0aW9uIn0=")
+
+	if _, err := c.Next(challenge); err == nil {
+		t.Fatal("Expected error from handling challenge")
+	}
+
+	if _, err := c.Next([]byte("")); err == nil {
+		t.Fatal("Expected error from handling challenge")
+	}
+
+}
