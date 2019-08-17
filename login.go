240c8404624e076f633766c16adbe96c7ac516b7
Art <wildart@users.noreply.github.com>
2019-08-17 04:31:25 -0400

    Add LOGIN client (#12)
    Some Microsoft SMTP servers don't support PLAIN, and require LOGIN.

--- login.go
+++ login.go
@@ -1,7 +1,41 @@
 package sasl
 
+import (
+	"bytes"
+)
+
 // The LOGIN mechanism name.
 const Login = "LOGIN"
+
+var expectedChallenge = []byte("Password:")
+
+type loginClient struct {
+	Username string
+	Password string
+}
+
+func (a *loginClient) Start() (mech string, ir []byte, err error) {
+	mech = "LOGIN"
+	ir = []byte(a.Username)
+	return
+}
+
+func (a *loginClient) Next(challenge []byte) (response []byte, err error) {
+	if bytes.Compare(challenge, expectedChallenge) != 0 {
+		return nil, ErrUnexpectedServerChallenge
+	} else {
+		return []byte(a.Password), nil
+	}
+}
+
+// A client implementation of the LOGIN authentication mechanism for SMTP,
+// as described in http://www.iana.org/go/draft-murchison-sasl-login
+//
+// It is considered obsolete, and should not be used when other mechanisms are
+// available. For plaintext password authentication use PLAIN mechanism.
+func NewLoginClient(username, password string) Client {
+	return &loginClient{username, password}
+}
 
 // Authenticates users with an username and a password.
 type LoginAuthenticator func(username, password string) error
--- login_test.go
+++ login_test.go
@@ -1,11 +1,40 @@
 package sasl_test
 
 import (
+	"bytes"
 	"errors"
 	"testing"
 
 	"github.com/emersion/go-sasl"
 )
+
+func TestNewLoginClient(t *testing.T) {
+	c := sasl.NewLoginClient("username", "Password:")
+
+	mech, resp, err := c.Start()
+	if err != nil {
+		t.Fatal("Error while starting client:", err)
+	}
+	if mech != "LOGIN" {
+		t.Error("Invalid mechanism name:", mech)
+	}
+
+	expected := []byte{117, 115, 101, 114, 110, 97, 109, 101}
+	if bytes.Compare(resp, expected) != 0 {
+		t.Error("Invalid initial response:", resp)
+	}
+
+	resp, err = c.Next(expected)
+	if err != sasl.ErrUnexpectedServerChallenge {
+		t.Error("Invalid chalange")
+	}
+
+	expected = []byte("Password:")
+	resp, err = c.Next(expected)
+	if bytes.Compare(resp, expected) != 0 {
+		t.Error("Invalid initial response:", resp)
+	}
+}
 
 func TestNewLoginServer(t *testing.T) {
 	var authenticated = false
